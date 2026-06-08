bd2e655d9103ede095f1457536fec8837994f615
lykkk060114 <2586356361@qq.com>
2026-06-08 10:09:58 +0800

    add : 增加了对于文件树的设置

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,25 +3,35 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 -- ---------- 插入模式 ---------- ---
-keymap.set("i", "jk", "<ESC>")
+keymap.set("i", "jk", "<ESC>", { desc = "quit i"})
+keymap.set("i", "kj", "<ESC>", { desc = "quit i"})
+
+--keymap.set("i", "<leader><leader>", "<ESC>")
 
 -- ---------- 视觉模式 ---------- ---
+
 -- 单行或多行移动
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv" , {desc = "move it down"})
+keymap.set("v", "K", ":m '<-2<CR>gv=gv" , {desc = "move it down"})
 
 -- ---------- 正常模式 ---------- ---
 -- 窗口
-keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
-keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
-
+keymap.set("n", "<leader>sv", "<C-w>v") -- 垂直新增窗口 
+keymap.set("n", "<leader>sh", "<C-w>s") -- 水平新增窗口
+-- 退出
+keymap.set("n", "<leader>q", "<cmd>q<CR>", {desc = "quit"})
+keymap.set("n", "<leader>wq", "<cmd>wq<CR>", {desc = "save && quit"})
+-- 保存
+keymap.set({ "n", "i", "v", "x", "s", "c" }, "<C-s>", "<cmd>w<CR>", { desc = "Save file" })
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- 切换buffer
-keymap.set("n", "<C-L>", ":bnext<CR>")
-keymap.set("n", "<C-H>", ":bprevious<CR>")
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,15 +1,103 @@
+-- 禁用netrw， 避免和nvim-tree冲突
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local function on_attach(bufnr)
+    local api = require("nvim-tree.api")
+
+    local function opts(desc)
+      return {
+        desc = "nvim-tree: " .. desc,
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        nowait = true,
+      }
+    end
+
+    -- 保留nvim-tree 默认快捷键
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- 自定义常用快捷键
+    vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+    vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+    vim.keymap.set("n", "v", api.node.open.vertical, opts("Open Vertical Split"))
+    vim.keymap.set("n", "s", api.node.open.horizontal, opts("Open Horizontal Split"))
+
+    vim.keymap.set("n", "a", api.fs.create, opts("Create"))
+    vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
+    vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
+    vim.keymap.set("n", "x", api.fs.cut, opts("Cut"))
+    vim.keymap.set("n", "c", api.fs.copy.node, opts("Copy"))
+    vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+
+    vim.keymap.set("n", "R", api.tree.reload, opts("Refresh"))
+    vim.keymap.set("n", "H", api.tree.toggle_hidden_filter, opts("Toggle Hidden"))
+    vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+end
+    
+      
+      
 require("nvim-tree").setup({
-  renderer = {
-    icons = {
-      show = {
-        file = false,
-        folder = false,
-        folder_arrow = false,
-        git = false,
-      },
+    on_attach = on_attach,
+
+    view = {
+        width = 30,
+        side = "left",
+        signcolumn = "yes",
     },
-  },
+
+    update_focused_file = {
+        enable = true,
+        update_root = false,
+    },
+
+    sync_root_with_cwd = true,
+
+    renderer = {
+        group_empty = true,
+        highlight_git = true,
+        highlight_opened_files = "name",
+
+        indent_markers = {
+            enable = true,
+        },
+
+        icons = {
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = true,
+            },
+        },
+    },
+
+    git = {
+        enable = true,
+        ignore = false,
+    },
+
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+    },
+
+    filters = {
+        dotfiles = false,
+        git_ignored = false,
+        custom = {
+            "^.git$",
+            "node_modules",
+            ".cache",
+        },
+    },
+
+    actions = {
+        open_file = {
+            quit_on_open = false,
+            resize_window = true,
+        },
+    },
 })
